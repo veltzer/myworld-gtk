9188628eceb6bf3b010c9105075338d8f052f840
Mark Veltzer <mark.veltzer@gmail.com>
2014-07-27 11:59:47 +0300

    minor changes

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -3,22 +3,10 @@
 # this scrip will install all the required packages that you need on
 # ubuntu to compile and work with this package.
 
-# packages and their reasons:
-# perl: old scripts
-# python: for scripts
-# python-mako: the templating system we use
-# python-mako-doc: documentation for the templating system
-# lilypond-doc: documentation for lilypond
-# lilypond: the main system we use.
-# timidity: convert midi to wav.
-# lame: encode ogg
-# pdfjam: pdfjoin for creaing pdfs.
-# python3-mako: mako for python3 (we are not really using it).
-
-import subprocess
+import subprocess # for check_call
 
 packs=[
-	'libgtkmm-3.0-dev',
+	'libgtkmm-3.0-dev', # for gtkmm that we use for the gui
 ]
 
 args=['sudo','apt-get','install']
